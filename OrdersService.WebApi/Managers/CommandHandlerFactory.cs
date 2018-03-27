0e5bc2f5ca2e3166a7d70d32035e0bb550961b1e
s. galiamov <sgalyamov@gmail.com>
2018-03-27 23:29:16 +0200

    added handler factory

---
+++ OrdersService.WebApi/Managers/CommandHandlerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OrdersService.BusinessLogic.Contracts.Commands;
+
+namespace OrdersService.WebApi.Managers
+{
+    public class CommandHandlerFactory : ICommandHandlerFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandHandlerFactory(IServiceProvider provider)
+        {
+            _serviceProvider = provider;
+        }
+
+        public ICommandHandler<TCommand> Resolve<TCommand>() where TCommand : ICommand
+        {
+            return (ICommandHandler<TCommand>) _serviceProvider.GetService(typeof(ICommandHandler<TCommand>));
+        }
+    }
+}
