181c054b6e1c5ea33d998f69391b9b784f70daf1
s. galiamov <sgalyamov@gmail.com>
2018-03-27 21:37:15 +0200

    added commands builder

--- OrdersService.WebApi/Controllers/OrdersCommandController.cs
+++ OrdersService.WebApi/Controllers/OrdersCommandController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OrdersService.BusinessLogic;
+using OrdersService.WebApi.Managers;
 using OrdersService.WebApi.Models;
 
 namespace OrdersService.WebApi.Controllers
@@ -6,10 +8,35 @@ namespace OrdersService.WebApi.Controllers
     [Route("api/[controller]")]
     public class OrdersCommandController : Controller
     {
-        [HttpPut("{id}")]
-        public OkObjectResult Put(string id, [FromBody] OrderInputModel data)
+        private readonly ICommandBuilder _commandBuilder;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public OrdersCommandController(
+            ICommandBuilder commandBuilder,
+            ICommandDispatcher commandDispatcher)
         {
-            return Ok(new { id });
+            _commandBuilder = commandBuilder;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(string id, [FromBody] OrderInputModel data)
+        {
+            var command = _commandBuilder.BuildUpdateOrderCommand(id, data);
+
+            _commandDispatcher.Execute(command);
+
+            return Ok(new {id});
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] OrderInputModel data)
+        {
+            var command = _commandBuilder.BuildAddOrderCommand(data);
+
+            _commandDispatcher.Execute(command);
+
+            return Ok();
         }
     }
 }
---
+++ OrdersService.WebApi/Managers/CommandBuilder.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using OrdersService.BusinessLogic.Commands;
+using OrdersService.WebApi.Models;
+
+namespace OrdersService.WebApi.Managers
+{
+    public class CommandBuilder : ICommandBuilder
+    {
+        private readonly IMapper _mapper;
+
+        public CommandBuilder(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public UpdateOrderCommand BuildUpdateOrderCommand(string id, OrderInputModel model)
+        {
+            var command = _mapper.Map<UpdateOrderCommand>(model);
+            command.Id = id;
+            return command;
+        }
+
+        public AddOrderCommand BuildAddOrderCommand(OrderInputModel model)
+        {
+            var command = _mapper.Map<AddOrderCommand>(model);
+
+            return command;
+        }
+    }
+}
