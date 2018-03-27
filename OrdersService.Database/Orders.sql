7d5f90421819fdc89c2ddb776747b99abab32316
s. galiamov <sgalyamov@gmail.com>
2018-03-27 21:38:28 +0200

    database project

---
+++ OrdersService.Database/Orders.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Orders] 
+(
+	[Id]				BIGINT				IDENTITY (1, 1) NOT NULL,
+	[CreationTimestamp]	DATETIMEOFFSET		CONSTRAINT [DF_Orders_CreationTimestamp] DEFAULT (GETUTCDATE()) NOT NULL,
+	[Version]			ROWVERSION			NOT NULL,
+
+	[DisplayId]			VARCHAR(50)			NOT NULL,
+	[Price]				MONEY				NOT NULL,
+	[CustomerName]		NVARCHAR (MAX)		NOT NULL,
+	[Address]			NVARCHAR (MAX)		NULL,
+
+    CONSTRAINT [PK_dbo.Application] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+CREATE NONCLUSTERED INDEX [IX_DisplayId] ON [dbo].[Orders]([DisplayId] ASC);
+GO
