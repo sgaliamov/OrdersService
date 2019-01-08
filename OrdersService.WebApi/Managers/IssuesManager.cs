de28649828452b389b2a902d20bd9ae368b048ce
s. galiamov <sgalyamov@gmail.com>
2019-01-08 09:18:21 +0100

    issue classes

---
+++ OrdersService.WebApi/Managers/IssuesManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersService.WebApi.Managers
+{
+    public sealed class IssuesManager
+    {
+    }
+}
---
+++ OrdersService.WebApi/Models/Issue.cs
@@ -0,0 +1,13 @@
+﻿namespace OrdersService.WebApi.Models
+{
+    public sealed class Issue
+    {
+        public FieldsCollection Fields { get; set; }
+        public string Id { get; set; }
+
+        public sealed class FieldsCollection
+        {
+            public string Summary { get; set; }
+        }
+    }
+}
