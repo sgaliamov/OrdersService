34e00b68f00cb074b877b6b15e3e0c151449faf5
s. galiamov <sgalyamov@gmail.com>
2019-01-08 13:17:42 +0100

    added ui validation

--- OrdersService.WebApi/Models/OrderInputModel.cs
+++ OrdersService.WebApi/Models/OrderInputModel.cs
@@ -5,7 +5,7 @@
         public string City { get; set; }
         public string Country { get; set; }
         public string CustomerName { get; set; }
-        public int? Number { get; set; }
+        public string Number { get; set; }
         public string Phone { get; set; }
         public string PostCode { get; set; }
         public decimal Price { get; set; }
