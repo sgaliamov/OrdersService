a75a645ca9fbfb3a65f04d3ed8829a93fe584081
s. galiamov <salavat.galiamov@albelli.com>
2019-08-19 13:28:33 +0200

    better OrdersQueryController

--- OrdersService.WebApi/Controllers/OrdersQueryController.cs
+++ OrdersService.WebApi/Controllers/OrdersQueryController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OrdersService.BusinessLogic.Contracts.DomainModels;
 using OrdersService.WebApi.Managers;
@@ -17,12 +18,17 @@ namespace OrdersService.WebApi.Controllers
         }
 
         [HttpGet("list/{page:int?}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Paged<OrderReadModel[]>>> GetByPage(int? page)
         {
             return await _ordersPresenter.GetByPageAsync(page ?? 1).ConfigureAwait(false);
         }
 
         [HttpGet("id/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<OrderReadModel>> GetById(string id)
         {
             var model = await _ordersPresenter.GetByIdAsync(id).ConfigureAwait(false);
